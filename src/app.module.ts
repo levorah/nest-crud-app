3a80e41bbae75efb2d26a637748e403cace1d4ed
levorah <levora004@gmail.com>
2025-03-03 18:17:43 +0000

    feat: service , controller , module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/app.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/app.bookmark';
+import { BookmarkModule } from './bookmark/app.module';
 import { PrismaModule } from './prisma/app.module';
 
 @Module({
