521d47c4d44e840f9d603f26250c31b62775684b
levorah <levora004@gmail.com>
2025-03-05 13:03:12 +0000

    feat: we used jwt for authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,13 @@ import { PrismaService } from "src/prisma/prisma.service";
 import * as argon from "argon2"
 import { AuthDto } from "./dto";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable({})
 
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) { }
 
     async signup(dto: AuthDto) {
         // we need to generate hash for our pass
@@ -15,7 +17,7 @@ export class AuthService {
         try {
 
             // then we need to save user data in db
-            const { hash, ...user } = await this.prisma.user.create({
+            const user = await this.prisma.user.create({
                 data: {
                     email: dto.email,
                     hash: hashed,
@@ -24,6 +26,7 @@ export class AuthService {
 
             // then we need return the saved user
             return user
+
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code === 'P2002') {
@@ -39,11 +42,6 @@ export class AuthService {
         const user = await this.prisma.user.findUnique({
             where: {
                 email: dto.email
-            },
-            select: {
-                email: true,
-                id: true,
-                hash: true,
             }
         })
 
@@ -56,16 +54,28 @@ export class AuthService {
         // compare password 
         const pwMatches = await argon.verify(user.hash, dto.password)
         // if password incorrect throw exception
-        if (user.hash) {
-            user.hash = 'hello'
-        }
         if (!pwMatches)
             throw new ForbiddenException(
                 'Credentials incorrect'
             )
 
         // send back user 
-        return user
+        return this.signToken(user.id, user.email)
+    }
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get("JWT_SECRET")
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: secret,
+        })
+        return {
+            access_token: token
+        };
     }
 
 }
